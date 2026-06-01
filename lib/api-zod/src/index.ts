01db42bd22d0bba3c16dfb0794769c26b03db7ce
ijeh <ijehifeany@gmail.com>
2026-06-01 12:32:17 +0100

    Resolve api-zod generated export collisions

--- lib/api-zod/src/index.ts
+++ lib/api-zod/src/index.ts
@@ -1,2 +1,7 @@
 export * from "./generated/api";
+export { LoginResponse, UploadContainersResponse } from "./generated/api";
 export * from "./generated/types";
+export type {
+  LoginResponse as LoginResponseType,
+  UploadContainersResponse as UploadContainersResponseType,
+} from "./generated/types";
